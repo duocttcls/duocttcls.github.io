fdc550eda97f09dbdc4bb6f4c2ea3ea3590ee641
Duoc Thai <duoc.thai@codelovers.vn>
2019-07-04 13:01:56 +0700

    movie

--- src/views/SlideMovie2.vue
+++ src/views/SlideMovie2.vue
@@ -3,17 +3,16 @@
       <div class="row">
         <div class="col-md-12 slide-start" :style="{ height: height + 'px' }">
           <div class="slide-start-1 slide-text">画面をクリックするとスライドショーを開始します<br>準黼ができましたら、画面をクリックしてスタートしてください</div>
-          <div class="slide-start-2 slide-text">Webdding Sweet memories</div>
+          <div class="slide-start-2 slide-text">Wedding Sweet memories</div>
           <div class="slide-start-3 slide-text">with</div>
           <div class="slide-start-4 start-img">
             <img :src="startImg" :height="height" width="100%">
           </div>
         </div>
         <div class="col-md-3">
-          <marquee direction="up" scrollamount="3">
-            <div class="slide-show-text" :style="{ height: height + 'px' }">
-              Special thanks.
-              Out Special guests. 
+          <marquee direction="up" scrollamount="3" id="slide-show-marquee" :style="{ height: height + 'px' }">
+            Special thanks.
+            Out Special guests. 
 
 
 
@@ -26,75 +25,75 @@
 
 
 
-              ☆新郎ゲスト☆
-              ◯会社上司◯
-              小林なつみ様
-              大森友以乃様
-              岩井三郎様
+            ☆新郎ゲスト☆
+            ◯会社上司◯
+            小林なつみ様
+            大森友以乃様
+            岩井三郎様
 
 
-              本日はありがとうございました
-              今後もご指導ご鞭撻のほどよろ
-              しくお願いいたします
+            本日はありがとうございました
+            今後もご指導ご鞭撻のほどよろ
+            しくお願いいたします
 
 
-              〇会社同期〇
-              有田明様
-              岩田勇一様
-              内野洋介様
+            〇会社同期〇
+            有田明様
+            岩田勇一様
+            内野洋介様
 
-              安井動様
-              平林沙耶様
-              山川豊様
-              伊藤様
-              松本右京様
-
-
-              素敵な同期に恵まれて幸せです
-              これからもよろしくね
+            安井動様
+            平林沙耶様
+            山川豊様
+            伊藤様
+            松本右京様
 
 
-              ◯大学友人◯
-              井出郁恵様
-              小西早紀様
-              永野真悠子様
-              松木丈雄様
-              西山一樹様
-              田島ちえみ様
-              白石勝久様
-              植田一徳様
-              田所さやか様
+            素敵な同期に恵まれて幸せです
+            これからもよろしくね
 
 
-              卒業しても定期的に集まる大切
-              な仲間
-              また飲もう!
+            ◯大学友人◯
+            井出郁恵様
+            小西早紀様
+            永野真悠子様
+            松木丈雄様
+            西山一樹様
+            田島ちえみ様
+            白石勝久様
+            植田一徳様
+            田所さやか様
 
 
-              ◯高校友人◯
-              小谷美月様
-              河原俊二様
-              竹田みあ様
-              小高マサカズ様
-              荻野怜奈様
-              木村良介様
-              百瀬瞬様
-              川瀬美紀様
-              小口勝久様
-              白木里奈様
-              羽田あさみ様
-              山本光様
-            </div>
+            卒業しても定期的に集まる大切
+            な仲間
+            また飲もう!
+
+
+            ◯高校友人◯
+            小谷美月様
+            河原俊二様
+            竹田みあ様
+            小高マサカズ様
+            荻野怜奈様
+            木村良介様
+            百瀬瞬様
+            川瀬美紀様
+            小口勝久様
+            白木里奈様
+            羽田あさみ様
+            山本光様
           </marquee>
         </div>
         <div class="col-md-9" :style="{ height: height + 'px' }">
           <div
             v-for="(url, index) in images"
             :class="`animation-image animation-image-${index}`"
-            v-if="index == active"
             :key="index"
           >
-            <img :src="url">
+            <div class="animation-boxshow">
+              <img :src="url">
+            </div>
           </div>
         </div>
     </div>
@@ -146,25 +145,47 @@
         }
       },
       mounted () {
+        document.getElementById('slide-show-marquee').stop();
         setInterval(() => {
           if(this.slideStart >= 5) {
+            $('.animation-image-' + this.active).animate({
+              opacity: 0
+            }, 1000, function() {
+              $(this).css({
+                opacity: 1,
+                display: 'none'
+              });
+            });
+
             if (this.active >= this.images.length - 1) {
-              this.active = -1;
-              $('.slide-start').show();
-              this.slideStart = 0;
+              this.active = 0;
+              // $('.slide-start').show();
+              // this.slideStart = 1;
+              // $('.slide-start-' + this.slideStart).show();
             } else {
               this.active = this.active + 1;
             }
+            $('.animation-image-' + this.active).show();
           } else {
-            $('.slide-start-' + this.slideStart).hide();
+            $('.slide-start-' + this.slideStart).animate({
+              opacity: 0
+            }, 2000, function() {
+              $(this).css({
+                opacity: 1,
+                display: 'none'
+              });
+            });
+
             this.slideStart = + this.slideStart + 1;
             $('.slide-start-' + this.slideStart).show();
             if(this.slideStart >= 5) {
               $('.slide-start').hide();
               this.active = 0;
+              $('.animation-image-0').show();
+              document.getElementById('slide-show-marquee').start();
             }
           }
-        }, 4000)
+        }, 6000)
       }
   };
 </script>
@@ -179,21 +200,25 @@
     .slide-text {
       text-align: center;
       height: 100%;
-      position: relative;
+      position: absolute;
       top: 47%;
       display: none;
       color: white;
       font-size: 36px;
       animation: fadein 4s;
+      width: 100%;
     }
     .slide-start-1 {
       display: block;
       font-size: 16px;
     }
     .start-img {
+      position: absolute;
+      display: none;
       animation: fadein 4s;
+      width: 100%;
     }
-    .slide-show-text {
+    #slide-show-marquee {
       white-space: pre;
       text-align: center;
       color: white;
@@ -201,20 +226,27 @@
       // letter-spacing: 2px;
     }
     .animation-image {
+      position: relative;
+      display: none;
       width: 100%;
       height: 100%;
       text-align: center;
-      -webkit-box-shadow: inset 3px 0px 50px 76px rgba(0,0,0,1);
-      -moz-box-shadow: inset 3px 0px 50px 76px rgba(0,0,0,1);
-      box-shadow: inset 3px 0px 50px 76px rgba(0,0,0,1);
-      z-index: 3;
-      img {
-        width: auto;
+      .animation-boxshow {
+        width: 100%;
         height: 100%;
-        max-width: 100%;
-        max-width: 100%;
-        position: relative;
-        z-index: -2;
+        -webkit-box-shadow: inset 3px 0px 50px 76px rgba(0,0,0,1);
+        -moz-box-shadow: inset 3px 0px 50px 76px rgba(0,0,0,1);
+        box-shadow: inset 3px 0px 50px 76px rgba(0,0,0,1);
+        z-index: 3;
+        img {
+          width: auto;
+          height: 100%;
+          max-width: 100%;
+          max-width: 100%;
+          position: relative;
+          z-index: -2;
+          animation: fadein 8s;
+        }
       }
     }
   }
